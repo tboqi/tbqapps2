5e320bca7587e37afea6bd8a2b240ccbe82ade1e
唐伯琦 <tangbq@tiens.com>
2017-02-22 14:14:09 +0800

    a

--- python/flask/hello.py
+++ python/flask/hello.py
@@ -2,8 +2,43 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/float/<float:post_id>')
+def show_float(post_id):
+    return 'float %f' % post_id
+
+@app.route('/path/<path:str>')
+def show_path(str):
+    return 'path %s' % str
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        do_the_login()
+    else:
+        show_the_login_form()
+
+def do_the_login():
+    pass
+
+def show_the_login_form():
+    pass
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
