c4b9067ff698805a1e1cac734c7e8af5f0f4a56c
唐伯琦 <tangbq@tiens.com>
2017-02-27 15:54:07 +0800

    a

--- python/flask/test170222/hello.py
+++ python/flask/test170222/hello.py
@@ -1,13 +1,15 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello World'
+    
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
