3e0fe20833039236e001d4db3fdf2ef30e09d04e
唐伯琦 <tangbq@tiens.com>
2017-03-01 16:45:21 +0800

    a

--- python/flask/test170228/wwwapp/user.py
+++ python/flask/test170228/wwwapp/user.py
@@ -4,3 +4,11 @@ from flask import abort, redirect, url_for, render_template
 @app.route('/user')
 def user_index():
     return render_template('user/index.html', cssurl=url_for('static', filename='style.css'))
+
+@app.route('/user/login')
+def user_login():
+    return render_template('user/index.html', cssurl=url_for('static', filename='style.css'))
+
+@app.route('/user/logout')
+def user_logout():
+    return render_template('user/index.html', cssurl=url_for('static', filename='style.css'))
