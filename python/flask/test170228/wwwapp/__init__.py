22ca996cc8c510e8dcecd5400488c7e8e4cc0ffe
唐伯琦 <tangbq@tiens.com>
2017-03-02 17:28:36 +0800

    a

--- python/flask/test170228/wwwapp/__init__.py
+++ python/flask/test170228/wwwapp/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from sqlalchemy import create_engine
+
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+engine = create_engine('mysql+pymysql://root:111111@localhost:3307/test', convert_unicode=True)
 
 import wwwapp.index
 import wwwapp.user
--- python/flask/test170228/wwwapp/index.py
+++ python/flask/test170228/wwwapp/index.py
@@ -1,6 +1,8 @@
-from wwwapp import app
+from wwwapp import app, engine
 from flask import abort, redirect, url_for, render_template, session
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user = engine.execute('select * from users where id = %s', ('1')).first()
+    print(user)
+    return render_template('index.html', user=user['name'])
