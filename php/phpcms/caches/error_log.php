86a975a01d1d7a7d5c2d952b054054b142080f01
唐伯琦 <tangbq@tiens.com>
2017-03-06 16:30:12 +0800

    a

--- php/phpcms/caches/error_log.php
+++ php/phpcms/caches/error_log.php
@@ -20,3 +20,15 @@
 <?php exit;?>06-06 21:32:06 | 2 | extract() expects parameter 1 to be array, null given | phpcms/modules/content/classes/html.class.php | 182
 <?php exit;?>06-06 21:32:14 | 2 | extract() expects parameter 1 to be array, null given | phpcms/modules/content/classes/html.class.php | 182
 <?php exit;?>06-07 16:57:24 | 2 | Invalid argument supplied for foreach() | phpcms/modules/video/player.php | 54
+<?php exit;?>03-06 13:07:32 | 2 | mysqli::mysqli(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>03-06 13:07:32 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
+<?php exit;?>03-06 13:07:32 | 2 | mysqli::mysqli(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) | phpcms\libs\classes\db_mysqli.class.php | 55
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::errno(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 397
+<?php exit;?>03-06 13:07:32 | 2 | db_mysqli::error(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 390
+<?php exit;?>03-06 13:07:32 | 2 | mysqli::close(): Couldn't fetch mysqli | phpcms\libs\classes\db_mysqli.class.php | 409
