6fb67d1e9ebe93761870341fe821fd8736259e4b
唐伯琦 <tangbq@tiens.com>
2017-03-07 11:54:48 +0800

    a

--- php/tbq.com/apps/admin/application/classes/Controller/Welcome.php
+++ php/tbq.com/apps/admin/application/classes/Controller/Welcome.php
@@ -2,36 +2,35 @@
 
 class Controller_Welcome extends Controller_Base {
 
-    protected $show_header = false;
+	protected $show_header = false;
 
-    public function action_index() {
-        $this->tpl = 'welcome/index';
+	public function action_index() {
+		$this->tpl = 'welcome/index';
 
-        $session = Session::instance();
-        $data = $session->as_array();
-        $this->data = $data;
-    }
+		$session = Session::instance();
+		$data = $session->as_array();
+		$this->data = $data;
+	}
 
-    public function action_main() {
-        die('Welcome/main');
-    }
-    public function action_indexdata() {
-        $tree = [
-            [
-                /*
-            "text" => '工具集',
-            "isexpand" => true,
-            "children" => [
-            ["url" => URL::site('tools/json2arr'), "text" => "json2arr"],
-            ["url" => URL::site('tools/serialize'), "text" => "反序列化"],
-            ["url" => URL::site('tools/md5'), "text" => "md5"],
-            ],*/
-            ],
-        ];
-        $data = [
-            'tree1' => $tree,
-            'adminTree' => [],
-        ];
-        die('var indexdata = ' . json_encode($data));
-    }
+	public function action_main() {
+		die('Welcome/main');
+	}
+	public function action_indexdata() {
+		$tree = [
+			[
+				"text" => '工具集',
+				"isexpand" => true,
+				"children" => [
+					["url" => URL::site('tools/json2arr'), "text" => "json2arr"],
+					["url" => URL::site('tools/serialize'), "text" => "反序列化"],
+					["url" => URL::site('tools/md5'), "text" => "md5"],
+				],
+			],
+		];
+		$data = [
+			'tree1' => $tree,
+			'adminTree' => [],
+		];
+		die('var indexdata = ' . json_encode($data));
+	}
 } // End Welcome
--- php/tbq.com/set.php
+++ php/tbq.com/set.php
@@ -2,4 +2,4 @@
 define('DB_NAME', 'tbq.com');
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
-define('DB_PASS', '111111');
+define('DB_PASS', '');
