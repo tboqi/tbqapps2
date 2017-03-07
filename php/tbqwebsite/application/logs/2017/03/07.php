f96f74a3cef3609dd5d41a365b43be34a505457b
唐伯琦 <tangbq@tiens.com>
2017-03-07 10:43:28 +0800

    a

---
+++ php/tbqwebsite/application/logs/2017/03/07.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2017-03-07 08:45:30 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2017-03-07 08:45:30 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 D:\tbqapp2\php\tbqwebsite\config.php(66): Kohana_Request->execute()
+#1 D:\tbqapp2\php\tbqwebsite\index.php(10): include('D:\\tbqapp2\\php\\...')
+#2 {main}
+2017-03-07 08:45:30 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2017-03-07 08:45:30 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 D:\tbqapp2\php\tbqwebsite\config.php(66): Kohana_Request->execute()
+#1 D:\tbqapp2\php\tbqwebsite\index.php(10): include('D:\\tbqapp2\\php\\...')
+#2 {main}
