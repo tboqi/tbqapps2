d09e86a304a2dc92ce625f389054d2d70d6631ba
唐伯琦 <tangbq@tiens.com>
2017-03-07 13:22:00 +0800

    a

--- php/tbq.com/apps/chentublog/application/config/database.php
+++ php/tbq.com/apps/chentublog/application/config/database.php
@@ -1,16 +1,16 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-return array (
-		'default' => array (
-				'type' => 'PDO',
-				'connection' => array (
-						'dsn' => 'mysql:host=127.0.0.1;dbname=chentublog',
-						'username' => 'root',
-						'password' => '111111',
-						'persistent' => FALSE 
-				),
-				'table_prefix' => '',
-				'charset' => 'utf8',
-				'caching' => FALSE 
-		) 
+return array(
+	'default' => array(
+		'type' => 'PDO',
+		'connection' => array(
+			'dsn' => 'mysql:host=127.0.0.1;dbname=chentublog',
+			'username' => 'root',
+			'password' => '',
+			'persistent' => FALSE,
+		),
+		'table_prefix' => '',
+		'charset' => 'utf8',
+		'caching' => FALSE,
+	),
 );
