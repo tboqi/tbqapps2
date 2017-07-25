6d3b521d44f42c34919589595b9c784d4f36a31d
tangbq <tangbq@tiens.com>
2017-07-25 17:30:35 +0800

    update from dell msysgit

---
+++ php/myFramework/apps/default/index.php
@@ -0,0 +1,70 @@
+<?php
+function resolveRequestUri() {
+    if (isset($_SERVER['HTTP_X_REWRITE_URL'])) {
+        // IIS
+        $requestUri = $_SERVER['HTTP_X_REWRITE_URL'];
+    } elseif (isset($_SERVER['REQUEST_URI'])) {
+        $requestUri = $_SERVER['REQUEST_URI'];
+        if ($requestUri !== '' && $requestUri[0] !== '/') {
+            $requestUri = preg_replace('/^(http|https):\/\/[^\/]+/i', '', $requestUri);
+        }
+    } elseif (isset($_SERVER['ORIG_PATH_INFO'])) {
+        // IIS 5.0 CGI
+        $requestUri = $_SERVER['ORIG_PATH_INFO'];
+        if (!empty($_SERVER['QUERY_STRING'])) {
+            $requestUri .= '?' . $_SERVER['QUERY_STRING'];
+        }
+    } else {
+        throw new InvalidConfigException('Unable to determine the request URI.');
+    }
+
+    return $requestUri;
+}
+
+function resolvePathInfo() {
+    $pathInfo = $this->getUrl();
+
+    if (($pos = strpos($pathInfo, '?')) !== false) {
+        $pathInfo = substr($pathInfo, 0, $pos);
+    }
+
+    $pathInfo = urldecode($pathInfo);
+
+    // try to encode in UTF8 if not so
+    // http://w3.org/International/questions/qa-forms-utf-8.html
+    if (!preg_match('%^(?:
+            [\x09\x0A\x0D\x20-\x7E]              # ASCII
+            | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+            | \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+            | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+            | \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+            | \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+            | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+            | \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+            )*$%xs', $pathInfo)
+    ) {
+        $pathInfo = utf8_encode($pathInfo);
+    }
+
+    $scriptUrl = $this->getScriptUrl();
+    $baseUrl = $this->getBaseUrl();
+    if (strpos($pathInfo, $scriptUrl) === 0) {
+        $pathInfo = substr($pathInfo, strlen($scriptUrl));
+    } elseif ($baseUrl === '' || strpos($pathInfo, $baseUrl) === 0) {
+        $pathInfo = substr($pathInfo, strlen($baseUrl));
+    } elseif (isset($_SERVER['PHP_SELF']) && strpos($_SERVER['PHP_SELF'], $scriptUrl) === 0) {
+        $pathInfo = substr($_SERVER['PHP_SELF'], strlen($scriptUrl));
+    } else {
+        throw new InvalidConfigException('Unable to determine the path info of the current request.');
+    }
+
+    if (substr($pathInfo, 0, 1) === '/') {
+        $pathInfo = substr($pathInfo, 1);
+    }
+
+    return (string) $pathInfo;
+}
+
+//http://myfw.com:10080/dira/welcome/index?arg=123
+$url = resolveRequestUri();
+var_dump($url);
