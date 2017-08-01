6a26815e9d7a285e76fc1cda83c4f31352808017
tangbq <tangbq@tiens.com>
2017-08-01 13:17:07 +0800

    update from dell msysgit

--- python/flask/blog/wwwapp/admin.py
+++ python/flask/blog/wwwapp/admin.py
@@ -5,4 +5,7 @@ from flask import abort, redirect, url_for, render_template, session
 @app.route('/admin')
 @app.route('/admin/index')
 def admin_index():
-    return render_template('admin/index.html')
+    if session['isLogin']:
+        return render_template('admin/index.html')
+    else:
+        return redirect(url_for('user_login'))
