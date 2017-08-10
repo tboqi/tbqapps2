47539a05a129553831ea5fd175c38299906aefe6
tangbq <tangbq@tiens.com>
2017-08-10 14:38:28 +0800

    update from dell msysgit

--- php/tbq.com/apps/chentublog/application/config/database.php
+++ php/tbq.com/apps/chentublog/application/config/database.php
@@ -1,16 +1,16 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
 return array(
-	'default' => array(
-		'type' => 'PDO',
-		'connection' => array(
-			'dsn' => 'mysql:host=127.0.0.1;dbname=chentublog',
-			'username' => 'root',
-			'password' => '',
-			'persistent' => FALSE,
-		),
-		'table_prefix' => '',
-		'charset' => 'utf8',
-		'caching' => FALSE,
-	),
+    'default' => array(
+        'type' => 'PDO',
+        'connection' => array(
+            'dsn' => 'mysql:host=10.0.75.1;dbname=chentublog',
+            'username' => 'root',
+            'password' => 'root',
+            'persistent' => FALSE,
+        ),
+        'table_prefix' => '',
+        'charset' => 'utf8',
+        'caching' => FALSE,
+    ),
 );
