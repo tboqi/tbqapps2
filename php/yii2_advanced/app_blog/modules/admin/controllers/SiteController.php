a21c9999469341b3c4fb5dec8e436df4e95681e4
tangbq <tangbq@tiens.com>
2017-08-18 12:56:57 +0800

    update from dell msysgit

--- php/yii2_advanced/app_blog/modules/admin/controllers/SiteController.php
+++ php/yii2_advanced/app_blog/modules/admin/controllers/SiteController.php
@@ -4,7 +4,7 @@ use yii\web\Controller;
 
 class SiteController extends Controller {
     public function actionIndex() {
-        return $this->render('test.html', ['test' => 'smarty']);
+        return $this->render('test.tpl', ['test' => 'smarty']);
         // return $this->render('greet');
     }
 }
