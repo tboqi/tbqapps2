e500cb56f3c09d711bc9bf5c6f073afbda1c6ce2
tangbq <tangbq@tiens.com>
2017-08-21 11:36:18 +0800

    update from dell msysgit

--- php/tbq.com/apps/chentublog/application/classes/Controller/Admin/Welcome.php
+++ php/tbq.com/apps/chentublog/application/classes/Controller/Admin/Welcome.php
@@ -1,8 +1,8 @@
 <?php
 class Controller_Admin_Welcome extends Controller {
     public function action_index() {
-        $view = View::factory('template_name.tpl');
-        $view->var = 'aaa';
+        $view = View::factory('template_name.html', ['var' => 'bbbbb']);
+        $view->var = '123123';
         $this->response->body($view);
     }
 }
--- php/tbq.com/modules/smarty3/classes/View.php
+++ php/tbq.com/modules/smarty3/classes/View.php
@@ -11,8 +11,8 @@
  */
 class View extends Kohana_View {
 
-public static $_smarty_is_loaded;
-public static $_global_bound_variables = array();
+    public static $_smarty_is_loaded;
+    public static $_global_bound_variables = array();
 
 /**
  * Sets the initial view filename and local data. Views should almost
@@ -25,13 +25,13 @@ public static $_global_bound_variables = array();
  * @return  void
  * @uses    View::set_filename
  */
-public function __construct($file = NULL, array $data = NULL) {
-  if ( self::is_smarty_template($file) ) {
-    throw new Kohana_Exception('Cannot initialise Smarty template :tpl as new View; use View::factory instead',
-    array(':tpl' => $file));
-  }
-  return parent::__construct($file, $data);
-}
+    public function __construct($file = NULL, array $data = NULL) {
+        if (self::is_smarty_template($file)) {
+            throw new Kohana_Exception('Cannot initialise Smarty template :tpl as new View; use View::factory instead',
+                array(':tpl' => $file));
+        }
+        return parent::__construct($file, $data);
+    }
 
 /**
  * Assigns a global variable by reference, similar to [View::bind], except
@@ -43,14 +43,14 @@ public function __construct($file = NULL, array $data = NULL) {
  * @param   mixed   referenced variable
  * @return  void
  */
-public static function bind_global($key, & $value) {
-  View::$_global_data[$key] = &$value;
-  View::$_global_bound_variables[$key] = TRUE;
-  if ( self::$_smarty_is_loaded ) {
-    Smarty::$global_tpl_vars[$key] = new Smarty_variable($value);
-    Smarty::$global_tpl_vars[$key]->value = &$value;
-  }
-}
+    public static function bind_global($key, &$value) {
+        View::$_global_data[$key] = &$value;
+        View::$_global_bound_variables[$key] = TRUE;
+        if (self::$_smarty_is_loaded) {
+            Smarty::$global_tpl_vars[$key] = new Smarty_variable($value);
+            Smarty::$global_tpl_vars[$key]->value = &$value;
+        }
+    }
 
 /**
  * Returns a new View object. If you do not define the "file" parameter,
@@ -62,21 +62,21 @@ public static function bind_global($key, & $value) {
  * @param   array   array of values
  * @return  View
  */
-public static function factory($file = NULL, array $data = NULL) {
-  if ( self::is_smarty_template($file) ) {
-    // backwards compatibility - translate smarty:template to template.tpl
-    if ( substr($file, 0, 7)=='smarty:') {
-      if ( strlen($file)==7 ) {
-        $file = NULL;
-      } else {
-        $file = substr($file, 7).'.tpl';
-      }
+    public static function factory($file = NULL, array $data = NULL) {
+        if (self::is_smarty_template($file)) {
+            // backwards compatibility - translate smarty:template to template.tpl
+            if (substr($file, 0, 7) == 'smarty:') {
+                if (strlen($file) == 7) {
+                    $file = NULL;
+                } else {
+                    $file = substr($file, 7) . '.tpl';
+                }
+            }
+            return Smarty_View::factory($file, $data);
+        } else {
+            return parent::factory($file, $data);
+        }
     }
-    return Smarty_View::factory($file, $data);
-  } else {
-    return parent::factory($file, $data);
-  }
-}
 
 /**
  * Identify Smarty template file
@@ -84,9 +84,9 @@ public static function factory($file = NULL, array $data = NULL) {
  * @param   string  template filename
  * @return  bool    TRUE iff file appears to be a Smarty template
  */
-public static function is_smarty_template($file) {
-  return substr($file, -4, 4)=='.tpl' || substr($file, 0, 7)=='smarty:';
-}
+    public static function is_smarty_template($file) {
+        return substr($file, -4, 4) == '.tpl' || substr($file, -5, 5) == '.html' || substr($file, 0, 7) == 'smarty:';
+    }
 
 /**
  * Sets the view filename.
@@ -97,13 +97,13 @@ public static function is_smarty_template($file) {
  * @return  View
  * @throws  Kohana_View_Exception
  */
-public function set_filename($file) {
-  if ( self::is_smarty_template($file) ) {
-    throw new Kohana_Exception('Cannot use set_filename to initialise Smarty template :tpl; use View::factory instead',
-    array(':tpl' => $file));
-  }
-  return parent::set_filename($file);
-}
+    public function set_filename($file) {
+        if (self::is_smarty_template($file)) {
+            throw new Kohana_Exception('Cannot use set_filename to initialise Smarty template :tpl; use View::factory instead',
+                array(':tpl' => $file));
+        }
+        return parent::set_filename($file);
+    }
 
 /**
  * Sets a global variable, similar to [View::set], except that the
@@ -115,20 +115,20 @@ public function set_filename($file) {
  * @param   mixed   value
  * @return  void
  */
-public static function set_global($key, $value = NULL) {
-  if ( is_array($key) ) {
-    foreach ($key as $key2=>$value) {
-      View::$_global_data[$key2] = $value;
-      if ( self::$_smarty_is_loaded ) {
-        Smarty_View::smarty_prototype()->assignGlobal($key2, $value);
-      }
+    public static function set_global($key, $value = NULL) {
+        if (is_array($key)) {
+            foreach ($key as $key2 => $value) {
+                View::$_global_data[$key2] = $value;
+                if (self::$_smarty_is_loaded) {
+                    Smarty_View::smarty_prototype()->assignGlobal($key2, $value);
+                }
+            }
+        } else {
+            View::$_global_data[$key] = $value;
+            if (self::$_smarty_is_loaded) {
+                Smarty_View::smarty_prototype()->assignGlobal($key, $value);
+            }
+        }
     }
-  } else {
-    View::$_global_data[$key] = $value;
-    if ( self::$_smarty_is_loaded ) {
-      Smarty_View::smarty_prototype()->assignGlobal($key, $value);
-    }
-  }
-}
 
 }
