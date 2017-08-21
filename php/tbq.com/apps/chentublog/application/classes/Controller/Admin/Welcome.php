ec95b6384e8d4a7599c81c58d713fdac06d6bbcd
tangbq <tangbq@tiens.com>
2017-08-21 15:39:50 +0800

    update from dell msysgit

--- php/tbq.com/apps/chentublog/application/classes/Controller/Admin/Welcome.php
+++ php/tbq.com/apps/chentublog/application/classes/Controller/Admin/Welcome.php
@@ -1,7 +1,7 @@
 <?php
 class Controller_Admin_Welcome extends Controller {
     public function action_index() {
-        $view = View::factory('template_name.html', ['var' => 'bbbbb']);
+        $view = View::factory('admin/index.html');
         $view->var = '123123';
         $this->response->body($view);
     }
