0d07fdd59088581df4b737ade2c9c02affdb4e3b
tangbq <tangbq@tiens.com>
2017-09-27 13:04:21 +0800

    update from dell msysgit

--- php/index.php
+++ php/index.php
@@ -1,9 +1,9 @@
 <?php
 //phpinfo();
 
-$conn = mysqli_connect('127.0.0.1', 'root', 'root');
-if ($conn) {
-    die('lian 接失败');
-} else {
-    die('no');
-}
+// $conn = mysqli_connect('127.0.0.1', 'root', 'root');
+// if ($conn) {
+//     die('lian 接失败');
+// } else {
+//     die('no');
+// }
