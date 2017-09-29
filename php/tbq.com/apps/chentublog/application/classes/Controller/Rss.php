4c8130350691a80411f4c92712c40bae45fc7f99
tangbq <tangbq@tiens.com>
2017-09-29 14:45:35 +0800

    update from dell msysgit

--- php/tbq.com/apps/chentublog/application/classes/Controller/Rss.php
+++ php/tbq.com/apps/chentublog/application/classes/Controller/Rss.php
@@ -1,11 +1,10 @@
 <?php
-class Controller_Rss extends Controller_Template {
-	
-	function action_index() {
-		$this->auto_render = false;
-		$article_model = new Model_Article ();
-		$view = View::factory( 'rss' );
-		$view->articles = $article_model->find(30, 0);
-		$this->response->body($view->render());
-	}
+class Controller_Rss extends Controller_base
+{
+
+    public function action_index()
+    {
+        $article_model = new Model_Article();
+        $this->display('rss.html', ['articles' => $article_model->find(30, 0)]);
+    }
 }
