30570f46360ace908f9c784902cda51ed3e0a910
tangbq <tangbq@tiens.com>
2017-09-29 15:23:09 +0800

    update from dell msysgit

--- php/tbq.com/apps/chentublog/application/classes/Controller/Admin/Welcome.php
+++ php/tbq.com/apps/chentublog/application/classes/Controller/Admin/Welcome.php
@@ -3,6 +3,6 @@ class Controller_Admin_Welcome extends Controller_Base
 {
     public function action_index()
     {
-        $this->display('admin/index.html');
+        $this->display('admin/welcome/index.html');
     }
 }
