88a514cc8236176fc7f5644a43c70c40a4f2c8cd
tangbq <tangbq@tiens.com>
2017-09-29 14:51:25 +0800

    update from dell msysgit

--- php/tbq.com/apps/chentublog/application/classes/Controller/Sitemap.php
+++ php/tbq.com/apps/chentublog/application/classes/Controller/Sitemap.php
@@ -1,12 +1,10 @@
 <?php
-class Controller_Sitemap extends Controller {
-	
-	function action_index() {
-		$this->auto_render = false;
-		$article_model = new Model_Article ();
-		$view = View::factory( 'sitemap' );
-		$view->articles = $article_model->find(300, 0);
-		$view->domain = URL::base($this->request);
-		$this->response->body($view->render());
-	}
+class Controller_Sitemap extends Controller_base
+{
+
+    public function action_index()
+    {
+        $article_model = new Model_Article();
+        $this->display('sitemap.html', ['domain' => URL::base(), 'articles' => $article_model->find(300, 0)]);
+    }
 }
