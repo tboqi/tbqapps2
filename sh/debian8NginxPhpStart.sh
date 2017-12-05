220f2aeea206401097ae8f75b776b641bdb42fac
tangbq <tangbq@tiens.com>
2017-12-05 10:55:23 +0800

    update from dell msysgit

--- sh/debian8NginxPhpStart.sh
+++ sh/debian8NginxPhpStart.sh
@@ -6,11 +6,14 @@ ln -s /work/d/code/phpcms.tiens.com/nginx.htaccess                  tiens_phpcms
 ln -s /work/d/code/www.tiens.com/nginx.htaccess                     tiens_www.conf
 ln -s /work/d/code/tjtjs-ecstore/nginx.access                       tiens_testshop_taijisun.conf
 ln -s /work/d/code/tiens_hualang/nginx.htaccess                     tiens_hualang.conf
+
 ln -s /work/d/code/koseven/apps/tiens_mqtest/nginx.htaccess         tiens_mqtest.conf
 
-ln -s /work/d/code/yii2apps/appblog/nginx.htaccess                  yii_appblog.conf
-ln -s /work/d/code/yii2apps/appplus/nginx.htaccess                  yii_plus.conf
-ln -s /work/d/code/yii2apps/apptiensmq/nginx.htaccess               apptiensmq.conf
+# ln -s /work/d/code/yii2apps/appblog/nginx.htaccess                  yii_appblog.conf
+ln -s /work/d/code/yii2advanced/apps/frontend/nginx.htaccess        yii2frontend.conf
+ln -s /work/d/code/yii2apps/appTiensMq/nginx.htaccess               appTiensMq.conf
+ln -s /work/d/code/yii2apps/appTiensZhaopinBackend/nginx.htaccess   appTiensZhaopinBackend.conf
+ln -s /work/d/code/yii2apps/appTiensZhaopinFrontend/nginx.htaccess  appTiensZhaopinFrontend.conf
 
 # ln -s /work/d/tbqapp2/php/ci/apps/www/nginx.htaccess                cidev.conf
 ln -s /work/d/tbqapp2/php/chentu.info.wp/nginx.htaccess             wp_chentu_info.conf
@@ -20,14 +23,17 @@ ln -s /work/d/code/koseven/apps/www/nginx.htaccess                  khn_www.conf
 ln -s /work/d/code/tbq_static/nginx.htaccess                        static.conf
 cd -
 
+rm /work/libs/php-5.3.29/var/log/php-fpm.log -rf
 cd /work/libs/php-5.3.29/var/log
 ln -s /work/d/docker_php_log/php-fpm.53.log php-fpm.log
 cd -
 
+rm /work/libs/php-5.6.30/var/log/php-fpm.log -rf
 cd /work/libs/php-5.6.30/var/log
 ln -s /work/d/docker_php_log/php-fpm.56.log php-fpm.log
 cd -
 
+rm /work/libs/php-7.1.6/var/log/php-fpm.log -rf
 cd /work/libs/php-7.1.6/var/log
 ln -s /work/d/docker_php_log/php-fpm.71.log php-fpm.log
 cd -
