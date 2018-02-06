d956b5f92c7d5ced390e7340a2751601c3efbc52
tangbq <tangbq@tiens.com>
2018-02-06 16:55:39 +0800

    update from dell msysgit

--- sh/debian8NginxPy3Php7.sh
+++ sh/debian8NginxPy3Php7.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
+/etc/init.d/php7.0-fpm start
+
 cd /work/d/code/flaskblog
 source .env/bin/activate
 uwsgi -d --ini uwsgi.ini  --thunder-lock  --enable-threads
-/etc/init.d/nginx restart
 deactivate
 cd -
+
+/etc/init.d/nginx restart
 
 /bin/bash
