fddee57e5a9f9f7f29ef39745f0a2f751a08afdd
tangbq <tangbq@tiens.com>
2018-03-12 10:24:45 +0800

    update from dell msysgit

--- sh/debian8NginxPy3Php7.sh
+++ sh/debian8NginxPy3Php7.sh
@@ -19,19 +19,16 @@ ln -s /work/d/code/tbq_static/nginx.htaccess            tbq_static.conf
 ln -s /work/d/code/yiicms_qiyezhan/nginx.conf           yiicms_qiyezhan.conf
 ln -s /work/d/code/yii2BasicAdmin/nginx.conf            yii2BasicAdmin.conf
 ln -s /work/d/code/yii2cms_liufee/nginx.conf            yii2cms_liufee.conf
-
-# ln -s /work/d/code/yii2_fecshop_app_advanced/nginx.conf fecshop.conf
-# ln -s /work/d/code/yii2_fecshop_app_advanced/fcgi.conf  /etc/nginx/fecshop_fcgi.conf
-# ln -s /work/d/code/yii2_fecshop_app_advanced/none.conf  /etc/nginx/fecshop_none.conf
+ln -s /work/d/code/lulucms2/nginx.conf                  lulucms2.conf
 
 # ln -s /work/d/code/yii2blog/nginx.htaccess                      yii_appblog.conf
 # ln -s /work/d/code/yii2advanced/apps/basic/nginx.htaccess       yii_advanced_basic.conf
 # ln -s /work/d/code/yii2advanced/apps/frontend/nginx.htaccess    yii2frontend.conf
 #
-# ln -s /work/d/tbqapp2/php/ci/apps/www/nginx.htaccess                cidev.conf
-# ln -s /work/d/tbqapp2/php/chentu.info.wp/nginx.htaccess             wp_chentu_info.conf
-# ln -s /work/d/code/koseven/apps/chentublog/nginx.htaccess           khn_chentublog.conf
-# ln -s /work/d/code/koseven/apps/www/nginx.htaccess                  khn_www.conf
+# ln -s /work/d/tbqapp2/php/ci/apps/www/nginx.htaccess            cidev.conf
+# ln -s /work/d/tbqapp2/php/chentu.info.wp/nginx.htaccess         wp_chentu_info.conf
+# ln -s /work/d/code/koseven/apps/chentublog/nginx.htaccess       khn_chentublog.conf
+# ln -s /work/d/code/koseven/apps/www/nginx.htaccess              khn_www.conf
 cd -
 
 /etc/init.d/nginx restart
